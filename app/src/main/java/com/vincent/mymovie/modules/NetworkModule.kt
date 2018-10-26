b6385adc728942393810ad832a2004f7de846afe
Vincent Woo <realplord@gmail.com>
2018-10-27 11:20:26 +1300

    Add network module.

---
+++ app/src/main/java/com/vincent/mymovie/modules/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.vincent.mymovie.modules
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule(private val baseUrl: String) {
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        val builder = GsonBuilder()
+        return builder.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+    }
+}
--- app/src/main/java/com/vincent/mymovie/services/OMDBMovieService.kt
+++ app/src/main/java/com/vincent/mymovie/services/OMDBMovieService.kt
@@ -4,7 +4,13 @@ import com.vincent.entities.Movie
 import com.vincent.usecases.service.IMovieService
 
 class OMDBMovieService : IMovieService {
+
+
     override suspend fun searchMovies(title: String): List<Movie> {
         return listOf()
+    }
+
+    companion object {
+        val BASE_URL = "https://raw.githubusercontent.com/catchnz/android-test/master/data/"
     }
 }
